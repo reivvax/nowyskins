18b2f57904e3b6c878200706e12f97b6194f20e0
cmielo <gracjanjr.barski@gmail.com>
2024-09-10 13:41:32 +0200

    Recommended prices are now displayed in sell prompt

--- public/scripts/sell.js
+++ public/scripts/sell.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const modal = document.getElementById('sellModal');
     const closeBtn = document.querySelector('.close');
     const confirmSellBtn = document.getElementById('confirm-sell');
-    const customPriceInput = document.getElementById('custom-price');
+    const customPriceInput = document.getElementById('modal-custom-price');
 
     document.querySelectorAll('.item-card').forEach(card => {
         const sellButton = card.querySelector('.sell-button');
@@ -45,7 +45,13 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('modal-item-image').alt = card.querySelector('.item-name').textContent;
             document.getElementById('modal-item-wear').textContent = card.querySelector('.paint-wear').textContent;
             document.getElementById('modal-item-seed').textContent = card.querySelector('.paint-seed').textContent;
-            document.getElementById('modal-item-price').textContent = card.querySelector('.item-price').textContent;
+            
+            let price = card.querySelector('.item-price').textContent;
+
+            if (price.startsWith("$"))
+                customPriceInput.value = price.substring(1);
+            else
+                customPriceInput.placeholder = price;
 
             confirmSellBtn.setAttribute('data-asset-id', sellButton.getAttribute('data-asset-id'));
             confirmSellBtn.setAttribute('data-class-id', sellButton.getAttribute('data-class-id'));
@@ -54,7 +60,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
             document.body.classList.add('no-scroll');
             modal.style.display = "block";
+            confirmSellBtn.focus();
         });
+    });
+
+    document.addEventListener('keydown', function(event) {
+        if (modal.style.display === "block" && event.key === 'Enter')
+            confirmSellBtn.click();
     });
 
     const closeModal = () => {
